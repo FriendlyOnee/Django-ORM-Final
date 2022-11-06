b3491f5db21b721c91499746a5b444f3f5bac753
FriendlyOne <danila.vadimovich@gmail.com>
2022-11-06 16:58:20 +0200

    M

--- NewsPaper/tasks.py
+++ NewsPaper/tasks.py
@@ -1,8 +1,9 @@
-#Тут команды, которые нужно вставить, чтобы выполнить команды
+#Тут команды, которые нужно вставить, чтобы выполнить задания 9-11
+from news.models import *
 
-UserProfile.objects.order_by("post_rating").reverse()
+UserProfile.objects.order_by("-post_rating")
 
 UserProfile.objects.get(pk=1).date_joined
 
-
+related_post = Post.objects.filter(author=UserProfile.objects.get(pk=1).user)
 
