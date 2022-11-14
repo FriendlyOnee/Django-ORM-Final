a0a5556f3b03364c513ad8caa5833e30f1381091
FriendlyOne <danila.vadimovich@gmail.com>
2022-11-14 14:33:15 +0200

    Final

--- NewsPaper/scrap.py
+++ NewsPaper/scrap.py
@@ -1,3 +1,9 @@
 from django.contrib.auth.models import User
 user = User.objects.last()
 post1 = Post.objects.create(author_id
+for post_i in Post.objects.order_by("-rating"):
+    print(post_i.author.username, post_i.date, post_i.header, post_i.preview())
+    print(Comment.objects.filter(post_i))
+    for comment_i in Comment.objects.filter(post_i):
+        print(comment_i.date, comment_i.rating, comment_i.content)
+    break
--- NewsPaper/tasks.py
+++ NewsPaper/tasks.py
@@ -1,9 +1,12 @@
 # Тут команды, которые нужно вставить, чтобы выполнить задания 9-11
 from news.models import *
 
-UserProfile.objects.order_by("-post_rating")
+for user_i in UserProfile.objects.order_by("-post_rating"):
+    print(user_i.user.username, user_i.post_rating, user_i.comment_rating)
+    break
 
-UserProfile.objects.get(pk=1).date_joined
-
-for e in Post.objects.filter(author=UserProfile.objects.get(pk=1).user):
-    print(e.header, e.preview(), e.rating)
+for post_i in Post.objects.order_by("-rating"):
+    print(post_i.author.username, post_i.date, post_i.header, post_i.preview())
+    for comment_i in Comment.objects.filter(pk=post_i.id):
+        print(comment_i.date, comment_i.author.username, comment_i.rating, comment_i.content)
+    break
