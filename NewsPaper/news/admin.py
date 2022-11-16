17dcd7ef7cd378b4c9e10b026aa36c9b5f3b3b36
FriendlyOne <danila.vadimovich@gmail.com>
2022-11-16 09:11:23 +0200

    ы

--- NewsPaper/news/admin.py
+++ NewsPaper/news/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile, SubHub, Post, Comment
 
-# Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(SubHub)
+admin.site.register(Post)
+admin.site.register(Comment)
