b63fd116c974e4cc01622e9b44a3bf4413468e1e
FriendlyOne <danila.vadimovich@gmail.com>
2022-11-19 11:30:14 +0200

    wip

--- TestProject/TestProject/settings.py
+++ TestProject/TestProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY ='-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
+    'SimpleApp'
 ]
 
 MIDDLEWARE = [
